755f1474101c48c83c56f2219c0bb7952e7cc5f0
Joseph Davis <josephdavis@Josephs-MacBook-Air-2.local>
2024-08-03 14:31:49 -0700

    added profile

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DecimalField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -7,3 +7,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class ProfileForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    age = DecimalField('Age ', validators=[DataRequired()])
+    field = StringField('Field', validators=[DataRequired()])
+    location = StringField('Location')
+    selfDescription = StringField('Self-Description',validators=[DataRequired()])
+    experience = StringField('Experience')
+    strength = StringField('Strengths/Skills')
+    goals = StringField('Aspirations')
+    submit = SubmitField('create account')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import current_app as app
 from flask import Flask, render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from app.forms import ProfileForm
+
 # from .models.db import db, User
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,6 +14,17 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('login'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    form = ProfileForm()
+    if form.validate_on_submit():
+        flash('User name: {}, Age: {}, Field: {}, Location: {}, selfDescription: {}, Experience: {}, Strength: {}, Goals: {}'.format(
+            form.name.data, form.age.data, form.field.data, form.location.data, form.selfDescription.data, form.experience.data, form.strength.data, form.goals.data))
+        return redirect(url_for('profile'))
+    return render_template('profile.html', title='Information', form=form)
+
+
 
 # @app.route('/users')
 # def list_users():
