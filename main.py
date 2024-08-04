e01d648ae61591ee681f89a22de724252714ed34
ezhong0 <edwardrzhong@gmail.com>
2024-08-04 14:00:36 -0700

    depop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from app import socketio, app
 
 # socketio = SocketIO(app, async_mode='eventlet')
 
-if __name__ == '__main__':
-    print("Starting the Flask application...")
-    socketio.run(app, host='0.0.0.0', port=5001)
-    print("Starting the Flask application...1")
+# if __name__ == '__main__':
+    # print("Starting the Flask application...")
+    # socketio.run(app, host='0.0.0.0', port=5001)
+    # print("Starting the Flask application...1")
